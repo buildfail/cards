c201092a90bb204277c0f02314e532f33812b81b
bstoilov <boriostoilov@gmail.com>
2019-10-03 12:43:23 +0300

    adding playes and animations

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,18 +1,75 @@
-import { Component } from '@angular/core';
-import { TweenLite } from 'gsap';
-
+import {
+  Component,
+  Input,
+  HostListener,
+  ElementRef,
+  ViewChild,
+  HostBinding,
+  OnInit
+} from "@angular/core";
+import { TweenLite, TweenMax, Sine, Power0 } from "gsap";
 
 @Component({
-  selector: 'app-card',
-  templateUrl: 'card.component.html',
-  styleUrls: ['card.component.css'],
+  selector: "app-card",
+  templateUrl: "card.component.html",
+  styleUrls: ["card.component.css"]
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  @HostBinding("style.pointer-events") pointerEvents = "auto";
+  @ViewChild("pk", { static: false }) pk: ElementRef;
 
-  constructor() { }
+  @Input() xOffset: number;
+  @Input() yOffset: number;
+  @Input() hoverStyle: string; // hover-top|hover-down|hover-left|hover-right
+  @Input() angle: number;
+  customStyle = {};
 
-  move(div) {
-    TweenLite.to(div, 0.2, { y: "-=200",ease: "Power0.easeNone" });
+  ngOnInit() {
+    if (this.angle) {
+      this.customStyle = { transform: "rotate(" + this.angle + "deg)" };
+    }
+  }
+  constructor() {}
+
+  @HostListener("click") onClick() {
+    let viewportOffset = this.pk.nativeElement.getBoundingClientRect();
+    let targetY = 0;
+    if (viewportOffset.top > window.innerHeight / 2) {
+      targetY = viewportOffset.top - window.innerHeight / 2;
+      targetY = -targetY;
+    } else {
+      targetY = window.innerHeight / 2 - viewportOffset.top;
+    }
+
+    let targetX = 0;
+    if (viewportOffset.left > window.innerWidth / 2) {
+      targetX = viewportOffset.left - window.innerWidth / 2;
+      targetX = -targetX;
+    } else {
+      targetX = window.innerWidth / 2 - viewportOffset.left;
+    }
+
+    let width = this.pk.nativeElement.offsetWidth;
+    let height = this.pk.nativeElement.offsetHeight;
+
+    const xoff = Math.floor(Math.random() * 1000) % 30;
+    const yoff = Math.floor(Math.random() * 1000) % 30;
+    const rotoff = Math.floor(Math.random() * 1000) % 180;
+    TweenMax.to(this.pk.nativeElement, 0.2, {
+      x: targetX - width / 2 + xoff,
+      y: targetY - height / 2 + yoff,
+      ease: Power0.ease
+    });
+    TweenLite.to(this.pk.nativeElement, .2, {
+      rotation: rotoff,
+      transformOrigin: "left 50%"
+    });
+    this.pointerEvents = "none";
   }
 
+  convertRemToPx(rem) {
+    return (
+      rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
+    );
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  playerLeftOffset = 35;
+  centralOffset = 45;
+  sideOffset = 30;
+  cardSpacingFactor = 0.7;
+
+  xPercent = 50;
+  yPercent = 50;
+
+  angleOffset = 4;
+
+  cards = [1, 2, 3, 4, 1, 1, 1, 1];
 
   constructor() {}
-
 }
